d104774afbebb10678c6d7abf67cd7fb57b85073
nao.u <nao.study0@gmail.com>
2025-12-14 17:06:52 +0900

    refactor: ヒアドキュメントから変更 #2

--- mysql/init/01-init-user.sh
+++ mysql/init/01-init-user.sh
@@ -7,13 +7,7 @@ DB_USERNAME=${DB_USERNAME:-habit_rpg}
 DB_PASSWORD=${DB_PASSWORD:-}
 
 # MySQLに接続してユーザーを作成
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" <<EOF
--- アプリケーション用の専用ユーザーを作成
-CREATE USER IF NOT EXISTS '${DB_USERNAME}'@'%' IDENTIFIED WITH mysql_native_password BY '${DB_PASSWORD}';
-
--- データベースへの全権限を付与
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${DB_USERNAME}'@'%';
-
--- 権限の変更を反映
-FLUSH PRIVILEGES;
-EOF
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e \
+  "CREATE USER IF NOT EXISTS '${DB_USERNAME}'@'%' IDENTIFIED WITH mysql_native_password BY '${DB_PASSWORD}'; \
+   GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${DB_USERNAME}'@'%'; \
+   FLUSH PRIVILEGES;"
